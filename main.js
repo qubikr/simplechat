8a045c7c938b79f7c200ae3a7b8b500e8b633da6
Vladimir Romanov <qubikr@gmail.com>
2016-01-17 17:22:44 +0000

    basic functional

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
  * @type Module server|Module server
  */
 var server = require('./lib/server.js');
+var socketServer = require('./lib/sockets.js');
 
 server.start();
+socketServer.start();
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,68 @@
-window.onload = function(){
-    
-    function Chat(socket) {
-        this.socket = socket;
-    };
-    
-    Chat.prototype.sendMessage = function (message) {
-        this.socket.emit('message', {text: message});
-    };
+(function(win, doc){
+
+    function Chat(SocketConstructor) {
+        var self = this,
+            socket = new SocketConstructor('ws://' + doc.location.hostname + ':8001'),
+            messageHandlers = [];
     
 
-}; 
+        self.init = function(){
+            
+            socket.onopen = function() {
+               socket.send('Test');
+            };
+            
+            socket.onmessage = function(event) {
+                for (var i = 0; i < messageHandlers.length; i++ ) {
+                    messageHandlers[i](event.data);
+                }
+            };
+        };
+        
+        self.sendMessage = function (message) {
+            socket.send(message);
+        };
+        
+        self.messageSubscribe = function (handler) {
+            if ('function' === typeof handler) {
+                messageHandlers.push(handler);
+            }
+        };
+    };
+    
+    
+    function ChatRoomController(chat) {
+        var self = this;
+        
+        var chat = chat,
+            historyContainer = doc.querySelector('.message-box'),
+            userList = doc.querySelector('.user-box'),
+            messageContainer = doc.querySelector('#send-message'),
+            sendButton = doc.querySelector('#send-button');
+
+        self.init = function(){
+            chat.init();
+            chat.messageSubscribe(function(message){
+                historyContainer.innerHTML += self.generateMessage(message);
+            });
+            
+            sendButton.onclick = function(event) {
+                event.preventDefault();
+                
+                chat.sendMessage(messageContainer.value);
+            };
+        };
+        
+        self.generateMessage = function(message) {
+            return '<div class="message">' + message + '</div>';
+        };
+    }
+    
+    win.onload = function(){
+        var chat = new Chat(win.WebSocket),
+            room = new ChatRoomController(chat);
+    
+        room.init();
+    };
+ 
+})(window, document);
