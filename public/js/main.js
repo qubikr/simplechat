072f9869796a423b6f4fd6535c7a2daf9c4eca4a
Vladimir Romanov <qubikr@gmail.com>
2016-01-18 02:03:59 +0000

    add alerts

--- public/js/main.js
+++ public/js/main.js
@@ -39,10 +39,9 @@
         
         self.username = '';
         
-        
         var chat = chat,
             historyContainer = doc.querySelector('.message-list'),
-            userList = doc.querySelector('.user-box'),
+            userList = doc.querySelector('.user-box-list'),
             messageContainer = doc.querySelector('#send-message'),
             sendButton = doc.querySelector('#send-button'),
             welcomePopup = doc.querySelector('#welcome-popup'),
@@ -65,11 +64,12 @@
 
         function handleMessages(response) {
             var data = JSON.parse(response),
-                html = '';  
+                html;  
         
             if ('messages' in data) {
+                html = '';
                 for (var i = 0; i < data.messages.length; i++) {
-                    html += self.generateMessage(data.messages[i]);
+                    html += generateMessage(data.messages[i]);
                 }
                 historyContainer.innerHTML += html;  
                 historyContainer.scrollIntoView(false);
@@ -81,17 +81,71 @@
                     case 'confirmName': self.setName(data.action.name); break;
                 }
             }
+            
+            if ('users' in data) {
+                html = '';
+                for (var j = 0; j < data.users.length; j++) {
+                    html += generateUserElement(data.users[j]);
+                }
+                
+                userList.innerHTML = html;
+            }
+            
+            if ('alerts' in data) {
+                html = '';
+                for (var k = 0; k < data.alerts.length; k++) {
+                    html += generateAlert(data.alerts[k]);
+                }
+                
+                historyContainer.innerHTML += html;
+            }
+
+            function generateMessage (message) {
+                return '<div class="message">' + 
+                        message.text + 
+                        '<div class="message-header">' +
+                            '<span class="username">' + (message.username || '') + '</span>' + 
+                            '<span class="date">' + generateDate(message.timestamp) + '</span>' + 
+                        '</div>' +
+                        '</div>';
+            };
+            
+            function generateUserElement (name) {
+                return '<div class="username">' + name + '</div>';
+            }
+            
+            function generateAlert (message) {
+                return '<div class="alert-message">' + 
+                            '<div class="date">' + 
+                                generateDate(message.timestamp) + 
+                            '</div>' + 
+                            message.text + 
+                        '</div>';
+            };
+            
+            function generateDate (timestamp) {
+                var timeString = '',
+                    date, currentDate;
+
+                if (!timestamp) {
+                    return '';
+                }
+                date = new Date(timestamp);
+                currentDate = new Date();
+
+                timeString += date.getHours() + ':' + ('0' + date.getMinutes()).slice(-2);
+
+                if (date.getDate() !== currentDate.getDate() || 
+                    date.getMonth() !== currentDate.getMonth() || 
+                    date.getFullYear() !== currentDate.getFullYear()) 
+                {
+                    timeString += ' ' + ('0' + date.getDate()).slice(-2) + '.' + ('0' + (date.getMonth() + 1)).slice(-2) + '.' + date.getFullYear();
+                }
+
+                return timeString;
+            }
         }
         
-        self.generateMessage = function(message) {
-            return '<div class="message">' + 
-                    message.text + 
-                    '<div class="message-header">' +
-                        '<span class="username">' + (message.username || '') + '</span>' + 
-                        '<span class="date">' + generateDate(message.timestamp) + '</span>' + 
-                    '</div>' +
-                    '</div>';
-        };
         
         self.changeName = function (name) {
             welcomePopup.className = welcomePopup.className.replace('hidden', '');
@@ -110,28 +164,6 @@
             welcomePopup.className = welcomePopup.className + ' hidden';
             self.userName = name;
         };
-        
-        function generateDate (timestamp) {
-            var timeString = '',
-                date, currentDate;
-        
-            if (!timestamp) {
-                return '';
-            }
-            date = new Date(timestamp);
-            currentDate = new Date();
-
-            timeString += date.getHours() + ':' + ('0' + date.getMinutes()).slice(-2);
-
-            if (date.getDate() !== currentDate.getDate() || 
-                date.getMonth() !== currentDate.getMonth() || 
-                date.getFullYear() !== currentDate.getFullYear()) 
-            {
-                timeString += ' ' + ('0' + date.getDate()).slice(-2) + '.' + ('0' + (date.getMonth() + 1)).slice(-2) + '.' + date.getFullYear();
-            }
-
-            return timeString;
-        }
     }
     
     win.onload = function(){
